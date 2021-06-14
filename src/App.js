16dbb795bf0c69db4232c105fd09858471219255
Ghost Orchid <kelsie.cummings23@gmail.com>
2021-06-14 11:49:42 -0400

    added react import statement to app.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Header from "./components/Header";
 import SearchForm from "./components/SearchForm";
 import SearchResultContainer from "./components/SearchResultContainer";
