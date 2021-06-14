b92eb94d510e61ff834fef78a305394b00dd1080
Ghost Orchid <kelsie.cummings23@gmail.com>
2021-06-14 11:44:56 -0400

    added header component, no css yet

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Employee Directory</h1>
+            <p>Click arrows on columns to filter, or use the search bar to narrow your results.</p>
+        </header>
+    )
+}
+
+export default Header;
